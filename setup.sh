ab799a8b84b5f8083ac9a6bb469c7459a4988907
Po-Han Chen <pohan@stanford.edu>
2022-04-08 17:41:02 -0700

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 # change this to where you want to install pdks and openlane
-export caravel_workspace=/tmp/pohan/workspace
+export caravel_workspace=/tmp/pohan/caravel_workspace
 
 # you need to export this whenever you start a new shell
 export OPENLANE_ROOT=${caravel_workspace}/openlane
