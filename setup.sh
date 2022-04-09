b81b1adae17ab9e76ce414f1dc650575965897f2
Po-Han Chen <pohan@stanford.edu>
2022-04-08 19:42:05 -0700

    update setup file as suggested by charles

--- setup.sh
+++ setup.sh
@@ -1,10 +1,5 @@
-# change this to where you want to install pdks and openlane
-export caravel_workspace=/tmp/pohan/caravel_workspace
-
-# you need to export this whenever you start a new shell
-export OPENLANE_ROOT=${caravel_workspace}/openlane
-export PDK_ROOT=${caravel_workspace}/pdks
-
-# install the pdk, caravel...etc if you haven't
-make setup
-
+export PDK_ROOT=/farmshare/home/classes/ee/372/PDKs/volare_pdk
+export OPENLANE_ROOT=/farmshare/home/classes/ee/372/openlane
+make install
+make install_mcw
+make simenv
